5f57affdbffd3b6032117f60a0770c3da6a64e2b
Mamdouh Ahmed Moustafa <Mamdouh64@gmail.com>
2017-09-24 17:36:39 +0200

    eliminated Unused imports

--- src/main/java/com/project/mini/controller/UserController.java
+++ src/main/java/com/project/mini/controller/UserController.java
@@ -5,18 +5,17 @@ import com.project.mini.dto.UserDTO;
 import com.project.mini.model.UserModel;
 import com.project.mini.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import javax.validation.ValidationException;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -28,6 +27,7 @@ public class UserController {
 
 
     @Autowired
+            @Qualifier("userService")
     UserService userService;
 
     @Secured({"ROLE_ADMIN" , "ROLE_USER"})
