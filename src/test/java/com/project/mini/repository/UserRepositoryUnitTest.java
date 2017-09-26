8937f3a2532539b042807fab2e95c21965cfb0ac
khaledAK <khaledahmed20130114@gmail.com>
2017-09-26 13:39:28 +0200

    adding test for user and note repositories

--- src/test/java/com/project/mini/repository/UserRepositoryUnitTest.java
+++ src/test/java/com/project/mini/repository/UserRepositoryUnitTest.java
@@ -1,5 +1,10 @@
 package com.project.mini.repository;
 
+import com.project.mini.model.UserModel;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,5 +15,52 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class UserRepositoryUnitTest {
     @Autowired
     private UserRepository userRepository;
+    private final Integer USER_ID = 1;
+    private final Integer USER_ID_NOT_VALID = 11;
+    private final String USER_EMAIL_NOT_VALID = "1@2.3" ;
+    private final String USER_EMAIL = "admin@orange.com" ;
+
+
+    @Test
+    public void findByIdTestValid() {
+        Assert.assertTrue(
+                userRepository.findById(USER_ID).isPresent()
+        );
+    }
+
+    @Test
+    public void findByIdTestNotValid() {
+        Assert.assertFalse(
+                userRepository.findById(USER_ID_NOT_VALID).isPresent()
+        );
+    }
+
+    @Test
+    public void findByEmailValid() {
+        Assert.assertTrue(
+                userRepository.findByEmail(USER_EMAIL).isPresent()
+        );
+    }
+
+    @Test
+    public void findByEmailNotValid() {
+        Assert.assertFalse(
+                userRepository.findByEmail(USER_EMAIL_NOT_VALID).isPresent()
+        );
+    }
+
+    @Test
+    public void existsByEmailValid() {
+        Assert.assertTrue(
+                userRepository.existsByEmail(USER_EMAIL)
+        );
+    }
+
+    @Test
+    public void existsByEmailNotValid() {
+        Assert.assertFalse(
+                userRepository.existsByEmail(USER_EMAIL_NOT_VALID)
+        );
+    }
 }
 
